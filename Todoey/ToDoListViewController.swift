ce497c341ebf8988bb88502f944adeef2a56fc01
ninjanam <namanhvu@gmail.com>
2018-01-14 09:39:32 -0600

    Add new item functionality complete

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy eggs", "Eat meat"]
+    var itemArray = ["Find Mike", "Buy eggs", "Eat meat"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
 
@@ -39,7 +40,7 @@ class ToDoListViewController: UITableViewController {
         
         //print(itemArray[indexPath.row])
         
-        tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark {
+       if tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -47,6 +48,41 @@ class ToDoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    // MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print(textField.text)
+            // error check here but we want to leave it for now
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
 
